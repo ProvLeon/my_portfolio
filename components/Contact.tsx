08d42074b71ea3dd824e22143651698fa1d34d0d
ProvLeon <providence.leonard@gmail.com>
2026-08-06 12:41:50 +0000

    feat: update contact section path to a curved line design

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -125,7 +125,7 @@ export default function Contact() {
             <rect x="0" y="0" width="1440" height="0" ref={clipRectRef} />
           </clipPath>
           <path
-            d="M 720 0 L 720 1000"
+            d="M 720 0 C 720 333, 480 333, 480 500 C 480 666, 720 666, 720 1000"
             stroke="var(--color-primary)"
             strokeWidth="2.5"
             fill="none"
